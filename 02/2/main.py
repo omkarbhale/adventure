d16f11219bc09f78cbbb4d12722ee516cd0c5240
Omkar Bhale <omkarbhale001@gmail.com>
2024-12-04 00:44:37 +0530

    --- Day 2: Red-Nosed Reports --- part 2

--- 02/2/main.py
+++ 02/2/main.py
@@ -1,49 +1,22 @@
-# returns true if triplet is safe
-# second return value is true if they are safe after removing middle value
-def triplet_safe(a, b, c, increasing):
-    is_safe = None
-    if increasing:
-        is_safe = c > b and b > a and c - b <= 3 and b - a <= 3
-    else:
-        is_safe = a > b and b > c and a - b <= 3 and b - c <= 3
-    
-    if is_safe:
-        return is_safe, None
-    
-    if increasing:
-        return is_safe, c > a and c - a <= 3
-    else:
-        return is_safe, a > c and a - c <= 3
-    
-
-def safe(levels, increasing, tolerated=False):
-    if levels[2] == 2:
-        pass
+def safe_helper(levels):
     if len(levels) == 1: return True
     if levels[0] == levels[1]: return False
 
-    # increasing = levels[1] > levels[0]
-
-    if len(levels) == 3: return triplet_safe(levels[0], levels[1], levels[2], increasing)[0] or triplet_safe(levels[0], levels[1], levels[2], increasing)[1]
-
-    # While loop because standard for-loop doesn't let me increment iterator
-    i = -1
-    while i + 1 < len(levels) - 2:
-        i += 1
-        a = levels[i]
-        b = levels[i + 1]
-        c = levels[i + 2]
-
-        is_safe, is_safe_tolerated = triplet_safe(a, b, c, increasing)
-        if is_safe: continue
-
-        if not tolerated and is_safe_tolerated:
-            tolerated = True
-            i += 1
-            continue
-        
-        return False
+    increasing = levels[1] > levels[0]
+    for i in range(len(levels) - 1):
+        if increasing and levels[i] >= levels[i + 1]: return False
+        if not increasing and levels[i + 1] >= levels[i]: return False
+        if abs(levels[i] - levels[i + 1]) > 3: return False
     return True
+
+def safe(levels):
+    if safe_helper(levels): return True
+
+    for i in range(len(levels)):
+        # Try damping each level
+        new_levels = levels[:i] + levels[i+1:]
+        if safe_helper(new_levels): return True
+    return False
 
 def main():
     safe_count = 0
@@ -52,7 +25,7 @@ def main():
             levels = list(map(int, f.readline().strip().split()))
             if len(levels) == 0: break
 
-            if safe(levels, True) or safe(levels[1:], True, tolerated=True) or safe(levels[:-1], True, tolerated=True) or safe(levels, False) or safe(levels[1:], False, tolerated=True) or safe(levels[:-1], False, tolerated=True):
+            if safe(levels):
                 print('- Safe')
                 safe_count += 1
             else:
